379f9a228e8f58b38ce710ad727d65af5af2eed1
Vishal Vaitheeswaran RK <vishal.vaitheeswaran@reveleer.com>
2026-03-02 17:42:38 +0530

    first commit

---
+++ ignition/modules/HelloWorld.ts
@@ -0,0 +1,7 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+export default buildModule("HelloWorldModule", (m) => {
+  const helloWorld = m.contract("HelloWorld");
+
+  return { helloWorld };
+});
